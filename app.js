833a1f8ae6837917282c7a52524a10447037697e
Pierre Ameduri <pierreameduri@gmail.com>
2024-09-21 11:34:07 -0300

    correçao de erro de escrita e gitignore

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function verificarChute() {
     if (chute == numeroSecreto){
         exibirTextoNaTela('h1', `Acertou!`);
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Vocês descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
